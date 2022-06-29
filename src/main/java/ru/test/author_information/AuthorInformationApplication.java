bd47b73dd34b9d644ffe31a544c1289ac7ff0dc3
ultar <lester_g@inbox.ru>
2022-06-29 12:54:38 +0300

    Init Commit

---
+++ src/main/java/ru/test/author_information/AuthorInformationApplication.java
@@ -0,0 +1,13 @@
+package ru.test.author_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorInformationApplication.class, args);
+    }
+
+}
